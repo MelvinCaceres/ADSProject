425e5f7a0ac3abe28dab4afba13a05f400d1d6ad
MelvinCaceres <frost.auditore25@gmail.com>
2024-03-23 12:14:30 -0600

    Se agrega CRUD para Estudiantes y Carreras

---
+++ adsProyecto/Models/Profesor.cs
@@ -0,0 +1,10 @@
+﻿namespace adsProyecto.Models
+{
+    public class Profesor
+    {
+        public int IdProfesor { get; set; }
+        public string NombresProfesor { get; set; }
+        public string ApellidosProfesor { get; set; }
+        public string Emil {  get; set; }
+    }
+}
